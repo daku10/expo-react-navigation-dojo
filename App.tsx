4b94062842e1834f9c8fa63a35eff22fdde1bce6
daku10 <daku.two@gmail.com>
2020-03-16 00:04:21 +0900

    done Nesting navigators

--- App.tsx
+++ App.tsx
@@ -1,85 +1,48 @@
-import React, { useState, useLayoutEffect } from "react";
-import { StyleSheet, Text, View, Button, Image } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createStackNavigator } from "@react-navigation/stack";
+import { View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 
-function LogoTitle() {
+const Tab = createBottomTabNavigator();
+
+function Home() {
   return (
-    <Image
-      style={{ width: 50, height: 50 }}
-      source={require("./assets/icon.png")}
-    />
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Messages" component={Messages} />
+    </Tab.Navigator>
   );
 }
 
-function HomeScreen({ navigation }) {
-  const [count, setCount] = useState(0);
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button onPress={() => setCount(c => c + 1)} title="Update coiunt" />
-      )
-    });
-  }, [navigation, setCount]);
-
+function Feed() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go o Details"
-        onPress={() => {
-          navigation.navigate("Details", {
-            itemId: 86,
-            otherParam: "anything you want here"
-          });
-        }}
-      />
-      <Text>Count: {count}</Text>
+    <View>
+      <Text>This is Feed</Text>
     </View>
   );
 }
 
-function DetailsScreen({ route, navigation }) {
-  const { itemId } = route.params;
-  const { otherParam } = route.params;
+function Messages() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-      <Text>itemId: {JSON.stringify(itemId)}</Text>
-      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
-      <Button
-        title="update title"
-        onPress={() => {
-          navigation.setOptions({ title: "Update Title!" });
-        }}
-      />
-      <Button
-        title="Go To Details... again"
-        onPress={() => {
-          navigation.push("Details", {
-            itemId: Math.floor(Math.random() * 100)
-          });
-        }}
-      />
-      <Button
-        title="Go to Home"
-        onPress={() => {
-          navigation.navigate("Home");
-        }}
-      />
-      <Button
-        title="Go back"
-        onPress={() => {
-          navigation.goBack();
-        }}
-      />
-      <Button
-        title="Go back to first screen in stack"
-        onPress={() => {
-          navigation.popToTop();
-        }}
-      />
+    <View>
+      <Text>This is Messages</Text>
+    </View>
+  );
+}
+
+function Profile() {
+  return (
+    <View>
+      <Text>This is Profile</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View>
+      <Text>This is Settings</Text>
     </View>
   );
 }
@@ -89,41 +52,11 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#f4511e"
-          },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold"
-          }
-        }}
-      >
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            headerTitle: props => <LogoTitle {...props} />
-          }}
-        />
-        <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          initialParams={{ itemId: 42 }}
-          options={({ route }) => ({ title: route.params.itemId })}
-        />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
